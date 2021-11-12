a2c0a7532c14e01fb6e88fb329eb577f042c970b
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-11-12 13:54:07 +0200

    added more sumbols for random

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 "use strict";
 
-const { alphabetLower, alphabetUpper } = require("./lib/alphabet");
-const { numbers } = require("./lib/numbers");
+const {
+  alphabetSortedFull,
+  alphabetLowerSorted,
+  alphabetLowerKeyboard,
+  alphabetLowerKeyboardInverted,
+  alphabetUpperSorted,
+  alphabetUpperKeyboard,
+  alphabetUpperKeyboardInverted,
+} = require("./lib/alphabet");
+const { numbersSorted, numbersInvert } = require("./lib/numbers");
 const { random } = require("./lib/utils/random");
 
 const MAX_LENGTH = 24;
-const symbols = `${alphabetUpper}${alphabetLower}${numbers}`;
+const symbols = [
+  alphabetSortedFull,
+  `${alphabetLowerSorted}${alphabetUpperSorted}`,
+  `${alphabetLowerKeyboard}${alphabetUpperKeyboard}`,
+  `${alphabetLowerKeyboardInverted}${alphabetUpperKeyboardInverted}`,
+  numbersSorted,
+  numbersInvert,
+];
 
 exports.juid = (minLength = MAX_LENGTH, maxLength) => {
   return generate(minLength, maxLength);
 };
 
-const generate = (minLength, maxLength) => {
+function generate(minLength, maxLength) {
   const length = getLength(minLength, maxLength);
   let id = "";
   for (let i = 0; i < length; i++) {
-    const randSymbs = symbols[random(0, symbols.length - 1)];
+    const randSymbs = getSumbols();
     id += randSymbs[random(0, randSymbs.length - 1)];
   }
   return id;
-};
+}
 
-const getLength = (minLength, maxLength) => {
+function getLength(minLength, maxLength) {
   return maxLength
     ? minLength > maxLength
       ? random(maxLength, minLength)
       : random(minLength, maxLength)
     : minLength;
-};
+}
+
+function getSumbols() {
+  return symbols[random(0, symbols.length - 1)];
+}
